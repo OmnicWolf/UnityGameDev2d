2dbe09ddc8536571ce5f30e7f185e705894d5ee1
Ryo Light <87935417+OmnicWolf@users.noreply.github.com>
2021-10-23 23:19:31 -0400

    Initial commit

---
+++ HelloUnity/Assets/PrintToConsole.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("The log is in no mood to roll. The log sits still.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Complete C# Unity Game Developer 2D
+
+## Environment
+- Unity Hub 3.0.0
+- Unity 2020.3.21f1
+- VS Code 1.61
