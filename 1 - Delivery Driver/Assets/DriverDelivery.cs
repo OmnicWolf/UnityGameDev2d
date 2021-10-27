f83bed6dd80da6f29392546abbe87b02aa74154f
Ryo Light <87935417+OmnicWolf@users.noreply.github.com>
2021-10-27 16:45:35 -0400

    P1. Delivery Driver - Added package delivery & boosts; project complete

--- 1 - Delivery Driver/Assets/DriverDelivery.cs
+++ 1 - Delivery Driver/Assets/DriverDelivery.cs
@@ -4,8 +4,47 @@ using UnityEngine;
 
 public class DriverDelivery : MonoBehaviour
 {
+    [SerializeField]
+    private Color32 packageColor = new Color32(31,176,60,255);
+
+    [SerializeField]
+    private Color32 defaultColor = new Color32(255,255,255,255);
+
+    private SpriteRenderer spriteRenderer;
+
+    private bool hasPackage;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Ka-chow!");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Color32 collisionObjectColor = collider.gameObject.GetComponent<SpriteRenderer>().color;
+
+        if(collider.tag == "Package" && !hasPackage)
+        {
+            hasPackage = true;
+            packageColor = collisionObjectColor;
+            Destroy(collider.gameObject, 0.2f);
+            spriteRenderer.color = collisionObjectColor;
+            // Debug.Log("Package picked up!");
+        }
+        else if(collider.tag == "Customer" && hasPackage && equals(packageColor, collisionObjectColor))
+        {
+            hasPackage = false;
+            spriteRenderer.color = defaultColor;
+            // Debug.Log("Package delivered!");
+        }
+    }
+
+    private bool equals(Color32 c1, Color32 c2) {
+        return c1.r == c2.r && c1.g == c2.g && c1.b == c2.b;
     }
 }
--- 1 - Delivery Driver/Assets/DriverMovement.cs
+++ 1 - Delivery Driver/Assets/DriverMovement.cs
@@ -5,20 +5,44 @@ using UnityEngine;
 public class DriverMovement : MonoBehaviour
 {
     [SerializeField] private float rotationFactor = 256f;
-    [SerializeField] private float speedFactor = 16f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+    [SerializeField] private float currentSpeedFactor = 16f;
+    private float timeSinceSpeedChange = 3f;
+    private bool isSpeedAltered = false;
+    [SerializeField] private readonly float normalSpeed = 16f;
+    [SerializeField] private readonly float slowSpeed = 8f;
+    [SerializeField] private readonly float boostSpeed = 32f;
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         /* Time.deltaTime = time elapsed between frames
          * Allows inputs to be performed independent of frames per second
          */
+
+        if(isSpeedAltered && timeSinceSpeedChange < 1.6f) {
+            this.timeSinceSpeedChange += Time.deltaTime;
+        } else {
+            this.currentSpeedFactor = this.normalSpeed;
+            this.isSpeedAltered = false;
+        }
+
         transform.Rotate(0, 0, -Input.GetAxis("Horizontal") * this.rotationFactor * Time.deltaTime);
-        transform.Translate(0, Input.GetAxis("Vertical") * this.speedFactor * Time.deltaTime, 0);
+        transform.Translate(0, Input.GetAxis("Vertical") * this.currentSpeedFactor * Time.deltaTime, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        this.currentSpeedFactor = this.slowSpeed;
+        this.timeSinceSpeedChange = 0f;
+        this.isSpeedAltered = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if(collider.tag == "SpeedBoost")
+        {
+            this.currentSpeedFactor = this.boostSpeed;
+            this.timeSinceSpeedChange = 0f;
+            this.isSpeedAltered = true;
+        }
     }
 }
